167e1bbba53f0f78156e08242d7bc0bc22d3ca8d
1mp3ra70r <122981852+1mp3ra70r@users.noreply.github.com>
2023-01-18 17:58:41 +0300

    Add files via upload

---
+++ CAPER/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace CAPER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelSize = new System.Windows.Forms.Label();
+            this.textBoxSize = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.labelBombCount = new System.Windows.Forms.Label();
+            this.labelTotalCount = new System.Windows.Forms.Label();
+            this.textBoxBomb = new System.Windows.Forms.TextBox();
+            this.textBoxTotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelSize
+            // 
+            this.labelSize.AutoSize = true;
+            this.labelSize.Font = new System.Drawing.Font("Arial Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSize.Location = new System.Drawing.Point(32, 46);
+            this.labelSize.Name = "labelSize";
+            this.labelSize.Size = new System.Drawing.Size(238, 38);
+            this.labelSize.TabIndex = 0;
+            this.labelSize.Text = "РАЗМЕР ПОЛЯ";
+            this.labelSize.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxSize
+            // 
+            this.textBoxSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSize.Location = new System.Drawing.Point(276, 46);
+            this.textBoxSize.Multiline = true;
+            this.textBoxSize.Name = "textBoxSize";
+            this.textBoxSize.Size = new System.Drawing.Size(100, 38);
+            this.textBoxSize.TabIndex = 1;
+            this.textBoxSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Arial Black", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(32, 96);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(203, 71);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // labelBombCount
+            // 
+            this.labelBombCount.AutoSize = true;
+            this.labelBombCount.Font = new System.Drawing.Font("Arial Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelBombCount.Location = new System.Drawing.Point(497, 53);
+            this.labelBombCount.Name = "labelBombCount";
+            this.labelBombCount.Size = new System.Drawing.Size(205, 28);
+            this.labelBombCount.TabIndex = 3;
+            this.labelBombCount.Text = "Количество бомб";
+            this.labelBombCount.Visible = false;
+            // 
+            // labelTotalCount
+            // 
+            this.labelTotalCount.AutoSize = true;
+            this.labelTotalCount.Font = new System.Drawing.Font("Arial Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTotalCount.Location = new System.Drawing.Point(476, 104);
+            this.labelTotalCount.Name = "labelTotalCount";
+            this.labelTotalCount.Size = new System.Drawing.Size(226, 28);
+            this.labelTotalCount.TabIndex = 4;
+            this.labelTotalCount.Text = "Количество плиток";
+            this.labelTotalCount.Visible = false;
+            // 
+            // textBoxBomb
+            // 
+            this.textBoxBomb.Font = new System.Drawing.Font("Arial Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxBomb.Location = new System.Drawing.Point(719, 44);
+            this.textBoxBomb.Multiline = true;
+            this.textBoxBomb.Name = "textBoxBomb";
+            this.textBoxBomb.ReadOnly = true;
+            this.textBoxBomb.Size = new System.Drawing.Size(69, 49);
+            this.textBoxBomb.TabIndex = 5;
+            this.textBoxBomb.Visible = false;
+            // 
+            // textBoxTotal
+            // 
+            this.textBoxTotal.Font = new System.Drawing.Font("Arial Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxTotal.Location = new System.Drawing.Point(719, 104);
+            this.textBoxTotal.Multiline = true;
+            this.textBoxTotal.Name = "textBoxTotal";
+            this.textBoxTotal.ReadOnly = true;
+            this.textBoxTotal.Size = new System.Drawing.Size(69, 49);
+            this.textBoxTotal.TabIndex = 6;
+            this.textBoxTotal.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxTotal);
+            this.Controls.Add(this.textBoxBomb);
+            this.Controls.Add(this.labelTotalCount);
+            this.Controls.Add(this.labelBombCount);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxSize);
+            this.Controls.Add(this.labelSize);
+            this.Name = "Form1";
+            this.Text = "Сапер";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSize;
+        private System.Windows.Forms.TextBox textBoxSize;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label labelBombCount;
+        private System.Windows.Forms.Label labelTotalCount;
+        private System.Windows.Forms.TextBox textBoxBomb;
+        private System.Windows.Forms.TextBox textBoxTotal;
+    }
+}
+
---
+++ CAPER/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CAPER
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int size;
+        char[,] mapChar;
+        Button[,] mapButtons;
+        bool[,] flags;
+        int bombCount = 0;
+        int unitCount = 0;
+        DialogResult result;
+
+        /// <summary>
+        /// Метод который проверяет количество бомб вокруг данной клетки
+        /// </summary>
+        /// <param name="a"> строка клетки </param>
+        /// <param name="b"> столбец клетки </param>
+        /// <returns></returns>
+        public string GetBombAroundCount(int a, int b)
+        {
+            int BombCount = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0) continue;
+                    try
+                    {
+                        if (mapChar[i + a, j + b] == '#') BombCount++;
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            string total = BombCount > 0 ? BombCount.ToString() : " ";
+            return total;
+        }
+
+        public bool[,] FlagFalse(bool[,] flags, int size)
+        {
+
+            for (int i = 0; i < flags.GetLength(0); i++)
+            {
+                for (int j = 0; j < flags.GetLength(1); j++)
+                {
+                    flags[i, j] = false;
+                }
+            }
+            return flags;
+        }
+
+        /// <summary>
+        /// Создаем массив чаров одинаковый по размерам с  
+        /// массивом кнопок чтобы кнопки изначально  
+        /// были пустые но по индексам  
+        /// бы значение соответствовало тексту на кнопке при нажатии
+        /// </summary>
+        /// <param name="map"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public char[,] charMapLoad(char[,] map, int size)
+        {
+            char[,] mapChar = new char[size, size];
+            Random rnd = new Random();
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    int unit = rnd.Next(0, 5);
+                    unitCount++;
+                    switch (unit)
+                    {
+                        case 0:
+                            map[i, j] = '#';
+                            bombCount++;
+                            break;
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:
+                            map[i, j] = ' ';
+                            break;
+                    }
+                }
+            }
+            mapChar = map;
+            return mapChar;
+        }
+
+        /// <summary>
+        /// Создание массива кнопок изначально пустых
+        /// Подписка их на ивент клика
+        /// </summary>
+        /// <param name="mapBtn"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public Button[,] MapLoad(Button[,] mapBtn, int size)
+        {
+            int height = (100 / size) * 3;
+            int width = (100 / size) * 3;
+
+            int x = 10;
+            int y = 10;
+            Button[,] mapButtons = new Button[size, size];
+            for (int i = 0; i < mapBtn.GetLength(0); i++)
+            {
+                for (int j = 0; j < mapBtn.GetLength(1); j++)
+                {
+                    mapBtn[i, j] = new Button();
+                    mapBtn[i, j].Font = new Font("ArialBlack", height / 2, FontStyle.Bold, GraphicsUnit.Point);
+                    mapBtn[i, j].Size = new Size(height, width);
+                    mapBtn[i, j].BackColor = Color.White;
+                    mapBtn[i, j].Text = "";
+                    mapBtn[i, j].Location = new Point(x, y);
+                    this.Controls.Add(mapBtn[i, j]);
+                    x += width + 1;
+                    mapBtn[i, j].MouseDown += button_MouseDown;
+                }
+                x = 10;
+                y += height + 1;
+            }
+            mapButtons = mapBtn;
+            return mapButtons;
+        }
+
+        /// <summary>
+        /// Ивент который происходит при нажатии кнопки старт
+        /// Включаем таймер
+        /// Выключаем надпись и текстбоксы
+        /// Создаем массив кнопок и массив значений пробела или мины
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void buttonStart_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+            try
+            {
+                size = int.Parse(textBoxSize.Text);
+            }
+            catch
+            {
+                size = 10;
+            }
+
+
+            labelSize.Visible = false;
+            textBoxSize.Visible = false;
+            buttonStart.Visible = false;
+
+            mapButtons = new Button[size, size];
+
+            mapChar = new char[size, size];
+
+            flags = new bool[size, size];
+
+
+
+            mapChar = charMapLoad(mapChar, size);
+            mapButtons = MapLoad(mapButtons, size);
+            FlagFalse(flags, size);
+
+            labelBombCount.Visible = true;
+            labelTotalCount.Visible = true;
+
+            textBoxBomb.Visible = true;
+            textBoxTotal.Visible = true;
+        }
+
+
+        private void button_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                
+                Button btn = new Button();
+                if (sender is Button)
+                {
+                    btn = sender as Button;
+                }
+                for (int i = 0; i < mapButtons.GetLength(0); i++)
+                {
+                    for (int j = 0; j < mapButtons.GetLength(1); j++)
+                    {
+                        if (btn == mapButtons[i, j])
+                        {
+                            if (!flags[i, j])
+                            {
+                                unitCount--;
+                                btn.Text = mapChar[i, j].ToString();
+                                if (btn.Text == " ")
+                                {
+                                    btn.BackColor = Color.Gray;
+                                    btn.ForeColor = Color.Snow;
+                                    btn.Text = GetBombAroundCount(i, j);
+
+                                }
+                                else
+                                {
+                                    bombCount--;
+
+                                    btn.BackColor = Color.DarkRed;
+                                    result = MessageBox.Show("Вы проиграли!!!",
+                                        "Поражение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                    if (result == DialogResult.OK)
+                                    {
+                                        Application.Restart();
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                Button btn = new Button();
+                if (sender is Button)
+                {
+                    btn = sender as Button;
+                }
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if(btn == mapButtons[i, j])
+                        {
+                            if (flags[i, j] && btn.BackColor != Color.Gray)
+                            {
+                                btn.BackColor = Color.White;
+                                flags[i, j] = false;
+                            }
+                            else if(!flags[i, j] && btn.BackColor != Color.Gray)
+                            {
+                                btn.BackColor = Color.Lime;
+                                flags[i, j] = true;
+                            }
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            textBoxBomb.Text = bombCount.ToString();
+            textBoxTotal.Text = unitCount.ToString();
+
+            if (textBoxTotal.Text == textBoxBomb.Text)
+            {
+                timer.Enabled = false;
+                result = MessageBox.Show("Вы победили", "Победа", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    Application.Restart();
+                }
+            }
+        }
+    }
+}
